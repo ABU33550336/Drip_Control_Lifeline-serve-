2de7c2ad0d56caf54421bce637faeb3878aae5a5
ABU33550336 <1915177605@qq.com>
2026-04-12 19:11:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ message: 'API working', path: req.url });
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const path = req.url || '/';
+
+  return res.status(200).json({
+    message: 'OK',
+    path: path,
+    method: req.method
+  });
 }
