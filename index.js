022e5b60ef378e839b249fcda8935e2e8b4f2f73
ABU33550336 <1915177605@qq.com>
2026-04-12 19:02:39 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  res.json({ 
-    message: 'API is working!',
-    path: req.url,
-    method: req.method,
-    env: {
-      hasAccountName: !!process.env.HUAWEI_ACCOUNT_NAME,
-      hasPassword: !!process.env.HUAWEI_PASSWORD,
-      hasDomain: !!process.env.HUAWEI_DOMAIN
-    }
-  });
+  res.json({ message: 'API working', path: req.url });
 };
